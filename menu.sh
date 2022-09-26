94705f616657f8b7c984662702aa0ff481adcc75
Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>
2022-09-26 11:49:12 +0530

    switch case
    Signed-off-by: Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>

--- menu.sh
+++ menu.sh
@@ -2,26 +2,24 @@
 # User can change this version
 export PYTHON_VERSION=3.7.7
 export PYTHON_MAJOR=3
-function pre_requisite()
+pre_requisite()
 {
     sudo apt install curl wget
 }
-function rust_install()
+rust_install()
 {
 
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 }
 # Updates or refreshes everything
-function update_system()
+update_system()
 {
   sudo apt update
   sudo apt upgrade
 
 }
-function python()
-{
-function important_lib()
+important_lib()
 {
   # for apt systems
 
@@ -30,7 +28,7 @@ function important_lib()
        libncurses5-dev libncursesw5-dev xz-utils tk-dev
 }
 # Building Python from Source
-function python_build()
+python_build()
 {
 curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
 tar -xvzf Python-${PYTHON_VERSION}.tgz
@@ -46,30 +44,31 @@ make
 sudo make install
 
 }
-function python_pip()
+python_pip()
 {
 	cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3curl -O https://bootstrap.pypa.io/get-pip.py
 	sudo /opt/python/${PYTHON_VERSION}/bin/python${PYTHON_MAJOR} get-pip.py
 
 }
-function npm_install()
+npm_install()
 {
   curl -qL https://www.npmjs.com/install.sh | sh
 }
-function volta_install()
+volta_install()
 {
     curl https://get.volta.sh | bash
 }
-function main_menu()
-{
 
-    # need some changes
-  wel_msg="Welcome to Project Turtle\nThis Project's main blah blah  blah" # this part has to be completed 
-  whiptail --msgbox --title "Project Turtle" "$wel_msg" 25 80 # you can also change the dimensions
-  if (whiptail --title "Python 🐍" --yesno "Do you wish to install Python" 8 78); then
-  echo "YES"
-  else
-  echo "NO"
-  fi
-}
-main_menu
+echo "Do you wish to install Python (Y/N)"
+read PYTHON_CHOICE
+case $PYTHON_CHOICE in
+
+    Y)
+        echo "Installing Python and PIP :-)"
+        python_build
+        python_pip
+        ;;
+    N)
+        echo "Skipping Python Install :-("
+        ;;
+esac
