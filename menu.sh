221f4ffe0ff90e8f1dada847c444ecc120654e17
Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>
2022-10-22 12:17:40 +0530

    Made changes to docker installation

--- menu.sh
+++ menu.sh
@@ -52,73 +52,27 @@ python_pip()
   pip$PYTHON_MAJOR -V
 
 }
-
-
 pip_req()
 {
   pip3 install -r requirements.txt
 }
 
-docker_install_repo()
+docker_install()
 {
-  sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-  echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  (lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-  sudo docker run SUCCESSFULLY INSTALLED
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo apt install docker-compose docker.io -y
 }
-docker_install_package()
-{
-  sudo dpkg -i ./containerd.io_<version>_<arch>.deb \
-  ./docker-ce_<version>_<arch>.deb \
-  ./docker-ce-cli_<version>_<arch>.deb \
-  ./docker-compose-plugin_<version>_<arch>.deb
-
-  sudo docker run SUCCESSFULLY INSTALLED
-}
-node_install()
-
-
 # volta is a javascript tool manager 
 volta_install()
-
 {
     curl https://get.volta.sh | bash
     volta install node 
     volta install yarn 
     volta install npm
 }
-
-docker_prereq()
-{
-  sudo apt install gnome-terminal
-  sudo apt remove docker-desktop
-  rm -r $HOME/.docker/desktop
-  sudo rm /usr/local/bin/com.docker.cli
-  sudo apt purge docker-desktop
-}
-docker_install()
-{
-    curl https://get.volta.sh | bash
-}
-go_install_snap()
-{
-  sudo snap install go --classic
-}
 go_install_apt()
 {
-  sudo apt update
-  sudo apt upgrade
 
   sudo apt search golang-go
   sudo apt search gccgo-go
